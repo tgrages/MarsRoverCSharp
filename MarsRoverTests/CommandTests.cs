dcb819bebd3bab28ffada4a0a46ce8bfcf982bb0
Tom Grages <tgrages_12@hotmail.com>
2021-07-02 11:37:00 -0400

    First 3 tests pass

--- MarsRoverTests/CommandTests.cs
+++ MarsRoverTests/CommandTests.cs
@@ -27,12 +27,18 @@ namespace MarsRoverTests
             Command newCommand = new Command("MOVE", 0);
             Assert.AreEqual(newCommand.CommandType, "MOVE");
         }
-
+        //ConstructSetsCommandType: You can make the command anything since you are just testing AreEqual. Does not have to be "Move".
         [TestMethod]
         public void ConstructorSetsInitialNewPositionValue()
         {
             Command newCommand = new Command("MOVE", 20);
             Assert.AreEqual(newCommand.NewPostion, 20);
+        }
+
+        [TestMethod]
+        public void ConstructorSetsInitialNewModeValue()
+        {
+
         }
 
     }
